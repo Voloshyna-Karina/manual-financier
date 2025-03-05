83cbd56133cb54fc9310ea0fcb8daef2e515ec61
Karina <94112873+Voloshyna-Karina@users.noreply.github.com>
2025-03-05 19:53:45 +0200

    add сalculator form

---
+++ front/src/components/Calculator/Calculator.jsx
@@ -0,0 +1,232 @@
+import React, { useState } from "react";
+import {
+    Button,
+    IconButton,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TextField,
+    Typography,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+import './Calculator.css';
+import Modal from '../Modal/Modal.jsx';
+
+const Calculator = () => {
+    const [table, setTable] = useState([
+        { id: 1, title: 'Оренда квартири', amountExpenses: '' },
+        { id: 2, title: 'Комуналка', amountExpenses: '' },
+        { id: 3, title: 'Транспорт', amountExpenses: '' },
+        { id: 4, title: 'Мобільний телефон', amountExpenses: '' },
+        { id: 5, title: 'Їжа', amountExpenses: '' },
+    ]);
+
+    const [openModal, setOpenModal] = useState(false);
+    const [openModalDelete, setOpenModalDelete] = useState(false);
+    const [rowToDelete, setRowToDelete] = useState(null);
+    const [errorsTable, setErrorsTable] = useState({});
+    const [errorsModal, setErrorsModal] = useState(false);
+
+    const [newRow, setNewRow] = useState({
+        title: '',
+        amountExpenses: ''
+    });
+
+    const handleAmountExpensesChange = (id, event) => {
+        const updatedRows = table.map(row =>
+            row.id === id ? { ...row, amountExpenses: event.target.value } : row
+        );
+        setTable(updatedRows);
+
+        if (errorsTable[id] && event.target.value) {
+            const newErrors = { ...errorsTable };
+            delete newErrors[id];
+            setErrorsTable(newErrors);
+        }
+    };
+
+    const handleNewRowChange = (event) => {
+        const { name, value } = event.target;
+        setNewRow(prevState => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    const addRow = () => {
+        if (!newRow.title || !newRow.amountExpenses) {
+            setErrorsModal(true);
+            return;
+        }
+
+        const updatedRows = [
+            ...table,
+            {
+                id: table.length + 1,
+                ...newRow,
+            },
+        ];
+        setTable(updatedRows);
+        handleCloseModal();
+    };
+
+    const handleOpenModal = () => {
+        setOpenModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
+        setNewRow({
+            title: '',
+            amountExpenses: ''
+        });
+        setErrorsModal(false);
+    };
+
+    const handleOpenModalDelete = (id) => {
+        setRowToDelete(id);
+        setOpenModalDelete(true);
+    };
+
+    const handleCloseModalDelete = () => {
+        setOpenModalDelete(false);
+        setRowToDelete(null);
+    };
+
+    const handleDeleteRow = () => {
+        if (rowToDelete) {
+            const updatedRows = table.filter(row => row.id !== rowToDelete);
+            setTable(updatedRows);
+            handleCloseModalDelete();
+        }
+    };
+
+    const handleConvert = () => {
+        const newErrors = {};
+        table.forEach(row => {
+            if (!row.amountExpenses) {
+                newErrors[row.id] = true;
+            }
+        });
+        setErrorsTable(newErrors);
+
+        // if (Object.keys(newErrors).length === 0) {
+        //     alert("Всі поля заповнені!");
+        // } else {
+        //     alert("Є порожні поля!");
+        // }
+    };
+
+    return (
+        <div className="calculator">
+            <Paper className={"calculator-form"} elevation={3}>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Категорія</TableCell>
+                                <TableCell>Сума витрат</TableCell>
+                                <TableCell>Дії</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {table.map(row => (
+                                <TableRow key={row.id}>
+                                    <TableCell className={"calculator-table-percent"}>
+                                        <Typography variant="body2" gutterBottom>
+                                            {row.title}
+                                        </Typography>
+                                    </TableCell>
+                                    <TableCell className="calculator-table-sum">
+                                        <TextField
+                                            size="small"
+                                            value={row.amountExpenses}
+                                            onChange={(e) => handleAmountExpensesChange(row.id, e)}
+                                            fullWidth
+                                            error={errorsTable[row.id]}
+                                            // label="Сума витрат"
+                                        />
+                                    </TableCell>
+                                    <TableCell className="calculator-table-delete">
+                                        <IconButton onClick={() => handleOpenModalDelete(row.id)}>
+                                            <DeleteIcon />
+                                        </IconButton>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+
+                    <div className="calculator-button-group">
+                        <Button
+                            variant="outlined"
+                            size="small"
+                            color="primary"
+                            onClick={handleOpenModal}
+                        >
+                            <AddIcon />
+                        </Button>
+
+                        <Button
+                            variant="outlined"
+                            size="medium"
+                            onClick={handleConvert}
+                        >
+                            Прорахувати
+                        </Button>
+                    </div>
+                </TableContainer>
+            </Paper>
+
+            <Modal isOpen={openModal} onClose={handleCloseModal}>
+                <TextField
+                    variant="outlined"
+                    name="title"
+                    label="Назва категорії"
+                    value={newRow.title}
+                    onChange={handleNewRowChange}
+                    fullWidth
+                    margin="normal"
+                    sx={{ mb: 2 }}
+                    error={errorsModal && !newRow.title}
+                    // helperText={errorsModal && !newRow.title ? "Це поле обов'язкове" : ""}
+                />
+                <TextField
+                    name="amountExpenses"
+                    label="Сума витрат"
+                    value={newRow.amountExpenses}
+                    onChange={handleNewRowChange}
+                    fullWidth
+                    margin="normal"
+                    sx={{ mb: 2 }}
+                    error={errorsModal && !newRow.amountExpenses}
+                    // helperText={errorsModal && !newRow.amountExpenses ? "Це поле обов'язкове" : ""}
+                />
+
+                <Button onClick={handleCloseModal}>Скасувати</Button>
+                <Button onClick={addRow} color="primary">Додати</Button>
+            </Modal>
+
+            <Modal isOpen={openModalDelete} onClose={handleCloseModalDelete}>
+                <DialogTitle>Видалення рядка</DialogTitle>
+                <DialogContent>
+                    <Typography>Ви впевнені, що хочете видалити цей рядок?</Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseModalDelete}>Скасувати</Button>
+                    <Button onClick={handleDeleteRow} color="error">Видалити</Button>
+                </DialogActions>
+            </Modal>
+        </div>
+    );
+};
+
+export default Calculator;
--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -1,22 +1,132 @@
 import {useDispatch, useSelector} from "react-redux";
-import {useEffect} from "react";
+import {Fragment, useEffect, useState} from "react";
 import {getCurrencies} from "../../actions/userActions.js";
 import generateApiToken from "../../helpers/generateApiToken.js";
+import {Box, Button, Step, StepButton, Stepper, Typography} from "@mui/material";
+
+const steps = ['Select campaign settings', 'Create an ad group', 'Create an ad'];
 
 const Header = ({...props}) => {
     const dispatch = useDispatch();
 
     const token = generateApiToken();
 
-    useEffect(() => {
-        dispatch(getCurrencies(token))
-            .then(res => console.log(res))
-            .catch(err => console.error(err));
-    }, [dispatch]);
+    // useEffect(() => {
+    //     dispatch(getCurrencies(token))
+    //         .then(res => console.log(res))
+    //         .catch(err => console.error(err));
+    // }, [dispatch]);
+
+
+    const [activeStep, setActiveStep] = useState(0);
+    const [completed, setCompleted] = useState({});
+
+    const totalSteps = () => {
+        return steps.length;
+    };
+
+    const completedSteps = () => {
+        return Object.keys(completed).length;
+    };
+
+    const isLastStep = () => {
+        return activeStep === totalSteps() - 1;
+    };
+
+    const allStepsCompleted = () => {
+        return completedSteps() === totalSteps();
+    };
+
+    const handleNext = () => {
+        const newActiveStep =
+            isLastStep() && !allStepsCompleted()
+                ? // It's the last step, but not all steps have been completed,
+                  // find the first step that has been completed
+                steps.findIndex((step, i) => !(i in completed))
+                : activeStep + 1;
+        setActiveStep(newActiveStep);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleStep = (step) => () => {
+        setActiveStep(step);
+    };
+
+    const handleComplete = () => {
+        setCompleted({
+            ...completed,
+            [activeStep]: true,
+        });
+        handleNext();
+    };
+
+    const handleReset = () => {
+        setActiveStep(0);
+        setCompleted({});
+    };
+
 
     return (
         <div>
-            <h1>Header</h1>
+            <Box sx={{ width: '100%' }}>
+                <Stepper nonLinear activeStep={activeStep}>
+                    {steps.map((label, index) => (
+                        <Step key={label} completed={completed[index]}>
+                            <StepButton color="inherit" onClick={handleStep(index)}>
+                                {label}
+                            </StepButton>
+                        </Step>
+                    ))}
+                </Stepper>
+                <div>
+                    {allStepsCompleted() ? (
+                        <Fragment>
+                            <Typography sx={{ mt: 2, mb: 1 }}>
+                                All steps completed - you&apos;re finished
+                            </Typography>
+                            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                                <Box sx={{ flex: '1 1 auto' }} />
+                                <Button onClick={handleReset}>Reset</Button>
+                            </Box>
+                        </Fragment>
+                    ) : (
+                        <Fragment>
+                            <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
+                                Step {activeStep + 1}
+                            </Typography>
+                            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                                <Button
+                                    color="inherit"
+                                    disabled={activeStep === 0}
+                                    onClick={handleBack}
+                                    sx={{ mr: 1 }}
+                                >
+                                    Back
+                                </Button>
+                                <Box sx={{ flex: '1 1 auto' }} />
+                                <Button onClick={handleNext} sx={{ mr: 1 }}>
+                                    Next
+                                </Button>
+                                {activeStep !== steps.length &&
+                                    (completed[activeStep] ? (
+                                        <Typography variant="caption" sx={{ display: 'inline-block' }}>
+                                            Step {activeStep + 1} already completed
+                                        </Typography>
+                                    ) : (
+                                        <Button onClick={handleComplete}>
+                                            {completedSteps() === totalSteps() - 1
+                                                ? 'Finish'
+                                                : 'Complete Step'}
+                                        </Button>
+                                    ))}
+                            </Box>
+                        </Fragment>
+                    )}
+                </div>
+            </Box>
         </div>
     );
 };
